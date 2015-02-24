5d030eb0221f331c82086a7193a44c34d44fa13f
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-24 23:06:10 +0000

    added template to iterate over projects collection

--- client/main.js
+++ client/main.js
@@ -1,18 +1,19 @@
-$(function() {
+Projects = new Mongo.Collection("projects");
 
+if (Meteor.isClient) {
 
+  Meteor.subscribe("projects");
 
-// Smooth scoll for anchor links
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
+  Template.project.helpers({
+    projects: function () {
+      return Projects.find({});
     }
   });
-});
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("projects", function () {
+      return Projects.find();
+    });
+}
+
