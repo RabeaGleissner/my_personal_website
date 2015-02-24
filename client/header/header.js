0da259b077c40754e286f1fb68a6faf1790eec32
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-24 21:39:32 +0000

    added images

---
+++ client/header/header.js
@@ -0,0 +1,16 @@
+$(function() {
+
+// displaying nav on scroll and click
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll >= 500) {
+            $("nav").removeClass("hide");
+        }
+  });
+
+  $('#enter').click(function(){
+      $("nav").removeClass("hide");
+  });
+  
+  });
--- client/main.js
+++ client/main.js
@@ -1,17 +1,6 @@
 $(function() {
 
-// displaying nav on scroll and click
 
-  $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) {
-            $("nav").removeClass("hide");
-        }
-  });
-
-  $('#enter').click(function(){
-      $("nav").removeClass("hide");
-  });
 
 // Smooth scoll for anchor links
   $('a[href*=#]:not([href=#])').click(function() {
