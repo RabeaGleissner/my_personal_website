d9328e1e527b3dd8a2632d3ddc1566d6c202fa26
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-25 11:44:45 +0000

    seperated projects and portfolio template into different filed, styling

--- client/main.js
+++ client/main.js
@@ -3,22 +3,23 @@ Projects = new Mongo.Collection("projects");
 if (Meteor.isClient) {
 
   Meteor.subscribe("projects");
-  
-
 
   Template.project.helpers({
     projects: function () {
-    console.log(this);
-
-    return Projects.find();
+    
+    console.log("returning " + Projects.find({}).count() + " projects");
+    return Projects.find({});
     }
   });
 
   }
 
+  
+
 if (Meteor.isServer) {
   Meteor.publish("projects", function () {
-      return Projects.find();
+      return Projects.find({});
+      console.log("returning " + Projects.find({}).count() + " projects on Sever side");
     });
 }
 
