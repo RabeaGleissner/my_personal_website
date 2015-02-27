8fab783c3641a0e05bd4124df12f4fe2c8230c89
Rabea Gleissner <rabeagleissner@gmail.com>
2015-02-27 11:25:00 +0000

    jquery selecting active description and displaying

--- client/body/projects.js
+++ client/body/projects.js
@@ -1,9 +1,20 @@
 Template.body.events({
-  "click #project-screenshot": function (event) {
+  "click #read-more": function (event) {
   event.preventDefault();
   console.log('clicked');
 
-  $('.project-description').toggleClass("hide");
+  var $this = $(event.target);
+      
+  console.log($this.parent());
+  
+  $($this).parent().parent().parent().parent().addClass('active');
+
+  showDescription();
 
   }
 });
+
+
+function showDescription() {
+    $(".project-item.active p").last().toggleClass( "hide" );
+}
