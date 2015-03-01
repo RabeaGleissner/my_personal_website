d06903dbd418f692ff2e6cc57c8a693667a4de3a
Rabea Gleissner <rabeagleissner@gmail.com>
2015-03-01 22:05:27 +0000

    showing active state of nav item

--- client/header/header.js
+++ client/header/header.js
@@ -1,23 +1,38 @@
 Template.body.events({
 
   "mouseup #home-nav": function () {
-  $("nav").hide();
-  $("nav").removeClass('fixed');
+    $("nav").hide();
+    $("nav").removeClass('fixed');
+  },
+  "mouseup #about-nav": function () {
+    $('nav a').removeClass('active');
+    $('#about-nav').addClass('active');
+  },
+  "mouseup #projects-nav": function () {
+    $('nav a').removeClass('active');
+    $('#projects-nav').addClass('active');
+  },
+  "mouseup #contact-nav": function () {
+    $('nav a').removeClass('active');
+    $('#contact-nav').addClass('active');
+  },
+  "mouseup a#enter": function () {
+    $('nav a').removeClass('active');
+    $('#about-nav').addClass('active');
   }
 
 });
 
 $(function() {
-    $(window).scroll(function() {
-      console.log('scroll')
-        var scroll = $(window).scrollTop();
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 400) {
+      $("nav").slideDown(1000);
+      $("nav").addClass('fixed');
+    } else {
+      $("nav").slideUp(1000);
+      $("nav").removeClass('fixed');
+    }
 
-        if (scroll >= 400) {
-            $("nav").slideDown(1000);
-            $("nav").addClass('fixed');
-        } else {
-          $("nav").slideUp(1000);
-          $("nav").removeClass('fixed');
-        }
-    });
+  });
 });
