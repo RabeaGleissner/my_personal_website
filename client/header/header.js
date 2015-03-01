ca22abea6d681d5def5be28a00ffd23b614a6507
Rabea Gleissner <rabeagleissner@gmail.com>
2015-03-01 21:15:51 +0000

    show nav on scroll

--- client/header/header.js
+++ client/header/header.js
@@ -1,17 +1,23 @@
 Template.body.events({
 
-  "mouseup #enter": function () {
-  console.log('ev def');
-
-  $("nav").slideDown(1000);
-  $("nav").addClass('fixed');
-
-  },
-
   "mouseup #home-nav": function () {
-  $("nav").slideUp(1000);
+  $("nav").hide();
   $("nav").removeClass('fixed');
-
   }
 
+});
+
+$(function() {
+    $(window).scroll(function() {
+      console.log('scroll')
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 400) {
+            $("nav").slideDown(1000);
+            $("nav").addClass('fixed');
+        } else {
+          $("nav").slideUp(1000);
+          $("nav").removeClass('fixed');
+        }
+    });
 });
