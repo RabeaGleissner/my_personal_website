73339bddd0ef7a2abfe1b1fdc88b60fdb848821b
Rabea Gleissner <rabeagleissner@gmail.com>
2015-05-10 21:10:13 +0100

    display website without intro screen

--- client/header/header.js
+++ client/header/header.js
@@ -1,9 +1,4 @@
 Template.body.events({
-
-  "mouseup #home-nav": function () {
-    $("nav").hide();
-    $("nav").removeClass('fixed');
-  },
   "mouseup #about-nav": function () {
     $('nav a').removeClass('active');
     $('#about-nav').addClass('active');
@@ -15,24 +10,5 @@ Template.body.events({
   "mouseup #contact-nav": function () {
     $('nav a').removeClass('active');
     $('#contact-nav').addClass('active');
-  },
-  "mouseup a#enter": function () {
-    $('nav a').removeClass('active');
-    $('#about-nav').addClass('active');
   }
-
-});
-
-$(function() {
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 400) {
-      $("nav").slideDown(1000);
-      $("nav").addClass('fixed');
-    } else {
-      $("nav").slideUp(1000);
-      $("nav").removeClass('fixed');
-    }
-
-  });
 });
