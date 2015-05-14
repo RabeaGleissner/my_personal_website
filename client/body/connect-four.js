dfbaeb4b1e765541b8d6e47bca7cc5eeb06f84db
Rabea Gleissner <rabeagleissner@gmail.com>
2015-05-14 23:25:06 +0100

    code to render a table in view

--- client/body/connect-four.js
+++ client/body/connect-four.js
@@ -1,3 +1,25 @@
-Meteor.startup(function () {
-  console.log('connect 4');
-});
+Template.connectFour.rendered = function(){
+   var table;
+   var tr;
+   var td;
+   
+   table = this.$(".table");
+
+   for (var i = 1; i < 8; i++){
+      var tr = $(document.createElement('tr'));
+      tr.addClass( "myClass yourClass" );
+      table.append(tr);
+
+      for (var j = 1; j < 8; j++){
+      var td = $(document.createElement('td'));
+    
+      tr.append(td);
+      }
+  }
+
+  $('#col1').click(function(){
+    console.log('clicked');
+  });
+
+
+}
