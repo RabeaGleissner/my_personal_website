f32df8920a63b63adcf09e505fed0897cf897d9e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-05-26 08:58:13 +0100

    added touch event to toggle navigation

--- client/header/header.js
+++ client/header/header.js
@@ -1,6 +1,9 @@
 Template.header.events({
   "click ul.navigation": function () {
     $('nav li').slideToggle();
-  }
+  },
+  "touchend ul.navigation": function () {
+    $('nav li').slideToggle();
+  },
 
 });
